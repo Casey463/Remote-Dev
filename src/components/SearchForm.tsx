3e13c61c03b2a651f3a2d5bcf1f12f9168be12c9
Casey463 <kfmconroy@gmail.com>
2024-10-25 15:05:28 -0700

    Set up simple fetch function for job items
    Job items are queried when searchText changes. Using new try catch syntax for ease of understanding.

--- src/components/SearchForm.tsx
+++ src/components/SearchForm.tsx
@@ -1,11 +1,40 @@
+import { useEffect, useState } from "react";
+
 export default function SearchForm() {
+  const [searchText, setSearchText] = useState("");
+
+  useEffect(() => {
+    if (!searchText) return;
+    try {
+      const search = async () => {
+        const response = await fetch(
+          `https://bytegrad.com/course-assets/projects/rmtdev/api/data?search=${searchText}`
+        );
+        const data = await response.json();
+        console.log(data.jobItems);
+      };
+
+      search();
+    } catch (error) {
+      console.error(`Error fetching data` + error);
+    }
+  }, [searchText]);
+
   return (
-    <form action="#" className="search">
+    <form
+      onSubmit={(e) => {
+        e.preventDefault();
+      }}
+      action="#"
+      className="search"
+    >
       <button type="submit">
         <i className="fa-solid fa-magnifying-glass"></i>
       </button>
 
       <input
+        value={searchText}
+        onChange={(e) => setSearchText(e.target.value)}
         spellCheck="false"
         type="text"
         required
