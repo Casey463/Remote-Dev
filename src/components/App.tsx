8c4f6fc75299db5b2c3cef456954667eee1c52bc
Casey463 <kfmconroy@gmail.com>
2024-10-25 21:15:20 -0700

    Updated imports

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,7 +1,17 @@
+import { useEffect, useState } from "react";
 import Background from "./Background";
 import Container from "./Container";
 import Footer from "./Footer";
-import Header from "./Header";
+import Header, { HeaderTop } from "./Header";
+import Logo from "./Logo";
+import BookmarksButton from "./BookmarksButton";
+import SearchForm from "./SearchForm";
+import Sidebar, { SidebarTop } from "./Sidebar";
+import JobItemContent from "./JobItemContent";
+import ResultsCount from "./ResultsCount";
+import JobList from "./JobList";
+import PaginationControls from "./PaginationControls";
+import SortingControls from "./SortingControls";
 
 function App() {
   const [jobItems, setJobItems] = useState([]);
