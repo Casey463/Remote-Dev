57b0f517e8bf8e5a5d797a14bc9602470cda9f0c
Casey463 <kfmconroy@gmail.com>
2024-10-25 21:14:59 -0700

    Added app state and data fetching function
    Added state to hold search text, and job item data. Added a data fetching function to grab job item data from API

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -4,6 +4,24 @@ import Footer from "./Footer";
 import Header from "./Header";
 
 function App() {
+  const [jobItems, setJobItems] = useState([]);
+  const [searchText, setSearchText] = useState("");
+  useEffect(() => {
+    if (!searchText) return;
+    try {
+      const search = async () => {
+        const response = await fetch(
+          `https://byteg  rad.com/course-assets/projects/rmtdev/api/data?search=${searchText}`
+        );
+        const data = await response.json();
+        setJobItems(data.jobItems);
+      };
+
+      search();
+    } catch (error) {
+      console.error(`Error fetching data` + error);
+    }
+  }, [searchText]);
   return (
     <>
       <Background />
