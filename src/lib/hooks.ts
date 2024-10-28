aaf6a8ee4f0c2374db3477900743a985e5555d2f
Casey463 <kfmconroy@gmail.com>
2024-10-28 00:43:18 -0700

    Added active ID and Job Item hooks
    Added get active ID from the browser, this ID can be used by Job Item hook in order to fetch a more detailed view.

--- src/lib/hooks.ts
+++ src/lib/hooks.ts
@@ -1,21 +1,58 @@
 import React, { useEffect, useState } from "react";
+import { JobItem, JobItemExpanded } from "./types.ts";
 import { JobItemContext } from "../providers/JobItemContextProvider";
-type useJobItemProps = {
-  searchText: string;
-};
+import { BASE_API_URL } from "./constants.ts";
 
-export function useJobItems({ searchText }: useJobItemProps) {
-  const [jobItems, setJobItems] = useState([]);
+export function useJobItem(id: number | null) {
+  const [jobItem, setJobItem] = useState<JobItemExpanded | null>(null);
+
+  useEffect(() => {
+    if (!id) return;
+    try {
+      const getJobData = async () => {
+        const response = await fetch(`${BASE_API_URL}/${id}`);
+        const data = await response.json();
+        setJobItem(data.jobItem);
+      };
+      getJobData();
+    } catch (error) {
+      console.error(`Error fetching job data: ${error}`);
+    }
+  }, [id]);
+
+  return jobItem;
+}
+
+export function useActiveId() {
+  const [activeID, setActiveID] = useState<null | number>(null);
+
+  useEffect(() => {
+    const handleHashChange = () => {
+      setActiveID(+window.location.hash.slice(1));
+    };
+    handleHashChange();
+
+    window.addEventListener("hashchange", handleHashChange);
+
+    return () => {
+      window.removeEventListener("hashchange", handleHashChange);
+    };
+  }, []);
+  return activeID;
+}
+
+export function useJobItems(searchText: string) {
+  const [jobItems, setJobItems] = useState<JobItem[]>([]);
   const [loading, setLoading] = useState(false);
+
+  const jobItemsSliced = jobItems.slice(0, 7);
 
   useEffect(() => {
     if (!searchText) return;
     try {
       const search = async () => {
         setLoading(true);
-        const response = await fetch(
-          `https://byteg  rad.com/course-assets/projects/rmtdev/api/data?search=${searchText}`
-        );
+        const response = await fetch(`${BASE_API_URL}?search=${searchText}`);
         const data = await response.json();
         setLoading(false);
         setJobItems(data.jobItems);
@@ -27,7 +64,7 @@ export function useJobItems({ searchText }: useJobItemProps) {
     }
   }, [searchText]);
 
-  return { jobItems, loading };
+  return [jobItemsSliced, loading] as const;
 }
 
 export const useJobItemContext = () => {
@@ -39,5 +76,3 @@ export const useJobItemContext = () => {
   }
   return context;
 };
-
-// This line should be inside a component or a custom hook.
