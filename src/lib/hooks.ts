25e762d6ea822eed854b59f1b2d763a21ca374c1
Casey463 <kfmconroy@gmail.com>
2024-11-04 13:15:25 -0800

    Updated useJobItem hook
    useJobItem hook now features react query to allow for caching

--- src/lib/hooks.ts
+++ src/lib/hooks.ts
@@ -2,28 +2,46 @@ import React, { useEffect, useState } from "react";
 import { JobItem, JobItemExpanded } from "./types.ts";
 import { JobItemContext } from "../providers/JobItemContextProvider";
 import { BASE_API_URL } from "./constants.ts";
+import { useQuery } from "@tanstack/react-query";
+
+// const [jobItem, setJobItem] = useState<JobItemExpanded | null>(null);
+// const [isLoading, setIsLoading] = useState(false);
+
+// useEffect(() => {
+//   if (!id) return;
+//   try {
+//     const getJobData = async () => {
+//       setIsLoading(true);
+//       const response = await fetch(`${BASE_API_URL}/${id}`);
+//       const data = await response.json();
+//       setIsLoading(false);
+//       setJobItem(data.jobItem);
+//     };
+//     getJobData();
+//   } catch (error) {
+//     console.error(`Error fetching job data: ${error}`);
+//   }
+// }, [id]);
+
+// return [jobItem, isLoading] as const;
 
 export function useJobItem(id: number | null) {
-  const [jobItem, setJobItem] = useState<JobItemExpanded | null>(null);
-  const [isLoading, setIsLoading] = useState(false);
-
-  useEffect(() => {
-    if (!id) return;
-    try {
-      const getJobData = async () => {
-        setIsLoading(true);
-        const response = await fetch(`${BASE_API_URL}/${id}`);
-        const data = await response.json();
-        setIsLoading(false);
-        setJobItem(data.jobItem);
-      };
-      getJobData();
-    } catch (error) {
-      console.error(`Error fetching job data: ${error}`);
+  const { data, isLoading } = useQuery(
+    ["job-item", id],
+    async () => {
+      const response = await fetch(`${BASE_API_URL}/${id}`);
+      const data = await response.json();
+      return data;
+    },
+    {
+      staleTime: 1000 * 60 * 60,
+      refetchOnWindowFocus: false,
+      enabled: Boolean(id),
+      onError: (error) => console.error(`Error fetching job data: ${error}`),
     }
-  }, [id]);
+  );
 
-  return [jobItem, isLoading] as const;
+  return [data?.jobItem, isLoading] as const;
 }
 
 export function useActiveId() {
