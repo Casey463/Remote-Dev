bc0420f4815874b06458c69c2f0d85894cf1e2ed
Casey463 <166182888+Casey463@users.noreply.github.com>
2024-11-23 11:19:47 -0800

    Added Bookmark storage and updater function
    Each bookmark for a job is stored by ID and can be toggled via the handleToggleBookmark function

--- src/providers/BookmarksContextProvider.tsx
+++ src/providers/BookmarksContextProvider.tsx
@@ -3,6 +3,14 @@ import React, { useState } from "react";
 export const BookmarksContext = React.createContext(null);
 
 export default function BookmarksContextProvider({ children }) {
+  const [bookmarkedIds, setBookmarkedIds] = useState<number[]>([]);
+  const handleToggleBookmark = (id: number) => {
+    if (bookmarkedIds.includes(id)) {
+      setBookmarkedIds((prev) => prev.filter((id) => id !== id));
+    } else {
+      setBookmarkedIds((prev) => [...prev, id]);
+    }
+  };
 
   return (
     <BookmarksContext.Provider value={{ bookmarkedIds, handleToggleBookmark }}>
