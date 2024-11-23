9eaf2e67585e2b7d5d4bbd653dd9bef53ebb1d94
Casey463 <166182888+Casey463@users.noreply.github.com>
2024-11-23 11:19:05 -0800

    Added BookmarkContextProvider
    Bookmark context provider allows for access to bookmarks and the handleToggleBookmark throughout the App. Consumed BookmarksContext for simpler access.

---
+++ src/providers/BookmarksContextProvider.tsx
@@ -0,0 +1,22 @@
+import React, { useState } from "react";
+
+export const BookmarksContext = React.createContext(null);
+
+export default function BookmarksContextProvider({ children }) {
+
+  return (
+    <BookmarksContext.Provider value={{ bookmarkedIds, handleToggleBookmark }}>
+      {children}
+    </BookmarksContext.Provider>
+  );
+}
+
+export const useBookmarks = () => {
+  const context = React.useContext(BookmarksContext);
+  if (!context) {
+    throw new Error(
+      "useBookmarks must be used within a BookmarksContextProvider"
+    );
+  }
+  return context;
+};
