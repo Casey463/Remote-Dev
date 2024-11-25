fa2955c5e19257841cfcb8c2e98cdf84b45bb98b
Casey463 <166182888+Casey463@users.noreply.github.com>
2024-11-24 19:32:49 -0800

    Added types for bookmarks provider

--- src/providers/BookmarksContextProvider.tsx
+++ src/providers/BookmarksContextProvider.tsx
@@ -2,9 +2,24 @@ import React, { useState } from "react";
 import React, { useEffect, useState } from "react";
 
 export const BookmarksContext = React.createContext(null);
+type BookmarksContextProviderProps = {
+  children: React.ReactNode;
+};
 
 export default function BookmarksContextProvider({ children }) {
   const [bookmarkedIds, setBookmarkedIds] = useState<number[]>([]);
+type BookmarksContext = {
+  bookmarkedIds: number[];
+  handleToggleBookmark: (id: number) => void;
+};
+
+export const BookmarksContext = React.createContext<BookmarksContext | null>(
+  null
+);
+
+export default function BookmarksContextProvider({
+  children,
+}: BookmarksContextProviderProps) {
   const [bookmarkedIds, setBookmarkedIds] = useState<number[]>(() =>
     JSON.parse(localStorage.getItem("bookmarkedIds") || "[]")
   );
