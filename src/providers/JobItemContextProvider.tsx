142e8bb16b448dc02739dd19e1f1a3880de9afa6
Casey463 <166182888+Casey463@users.noreply.github.com>
2025-04-07 17:39:53 -0700

    Added JobItem state to JobItemContextProvider
    This provider now hosts all JobItem state and derived state

--- src/providers/JobItemContextProvider.tsx
+++ src/providers/JobItemContextProvider.tsx
@@ -1,22 +1,79 @@
 import React, { createContext, useState } from "react";
-import { JobItem } from "../lib/types";
+import { JobItem, PageDirection, SortBy } from "../lib/types";
+import { RESULTS_PER_PAGE } from "../lib/constants";
+import { useSearchQuery, useSearchTextContext } from "../lib/hooks";
 
 type JobItemProps = {
   children: React.ReactNode;
 };
 
 type JobItemContext = {
-  jobItems: JobItem[];
-  setJobItems: React.Dispatch<React.SetStateAction<JobItem[]>>;
+  jobItems: JobItem[] | undefined;
+  jobItemsSortedAndSliced: JobItem[];
+  loading: boolean;
+  totalNumberOfResults: number;
+  totalNumberOfPages: number;
+  handleChangePage: (direction: PageDirection) => void;
+  handleChangeSortBy: (newSortBy: SortBy) => void;
+  currentPage: number;
+  sortBy: SortBy;
 };
 
 export const JobItemContext = createContext<JobItemContext | null>(null);
 
 export default function JobItemContextProvider({ children }: JobItemProps) {
-  const [jobItems, setJobItems] = useState<JobItem[]>([]);
+  const { debouncedSearchText } = useSearchTextContext();
+
+  // State
+  const { jobItems, loading } = useSearchQuery(debouncedSearchText);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [sortBy, setSortBy] = useState<SortBy>("relevant");
+
+  // Derived state
+  const jobItemsSorted = [...(jobItems || [])]?.sort((a, b) => {
+    if (sortBy === "relevant") {
+      return b.relevanceScore - a.relevanceScore;
+    } else if (sortBy === "recent") {
+      return a.daysAgo - b.daysAgo;
+    }
+    return 0;
+  });
+  const jobItemsSortedAndSliced = jobItemsSorted.slice(
+    currentPage * RESULTS_PER_PAGE - RESULTS_PER_PAGE,
+    currentPage * RESULTS_PER_PAGE
+  );
+  const totalNumberOfResults = jobItems?.length || 0;
+  const totalNumberOfPages = totalNumberOfResults / RESULTS_PER_PAGE || 0;
+
+  // Event handlers
+
+  const handleChangePage = (direction: PageDirection) => {
+    if (direction === "next") {
+      setCurrentPage((prev) => prev + 1);
+    } else if (direction === "prev") {
+      setCurrentPage((prev) => prev - 1);
+    }
+  };
+
+  const handleChangeSortBy = (newSortBy: SortBy) => {
+    setSortBy(newSortBy);
+    setCurrentPage(1);
+  };
   return (
-    <JobItemContext.Provider value={{ jobItems, setJobItems }}>
-      {children}
+    <JobItemContext.Provider
+      value={{
+        loading,
+        jobItems,
+        jobItemsSortedAndSliced,
+        totalNumberOfResults,
+        totalNumberOfPages,
+        handleChangePage,
+        handleChangeSortBy,
+        currentPage,
+        sortBy,
+      }}
+    >
+      ,{children}
     </JobItemContext.Provider>
   );
 }
