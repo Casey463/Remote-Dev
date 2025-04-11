6c234e32a9d25075ad285caa589cc00d7ae2f40b
Casey463 <166182888+Casey463@users.noreply.github.com>
2025-04-10 18:20:07 -0700

    Updated Main to include all new context providers

--- src/main.tsx
+++ src/main.tsx
@@ -4,13 +4,23 @@ import App from "./components/App.tsx";
 import "./index.css";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import BookmarksContextProvider from "./providers/BookmarksContextProvider.tsx";
+import ActiveIDContextProvider from "./providers/ActiveIDContexrProvider.tsx";
+import SearchTextContextProvider from "./providers/SearchTextContextProvider.tsx";
+
+import JobItemContextProvider from "./providers/JobItemContextProvider.tsx";
 
 const queryClient = new QueryClient();
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
       <BookmarksContextProvider>
-        <App />
+        <ActiveIDContextProvider>
+          <SearchTextContextProvider>
+            <JobItemContextProvider>
+              <App />
+            </JobItemContextProvider>
+          </SearchTextContextProvider>
+        </ActiveIDContextProvider>
       </BookmarksContextProvider>
     </QueryClientProvider>
   </React.StrictMode>
